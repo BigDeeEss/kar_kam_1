cfadce674b39b43b9966469f8216cb8aad7a3673
Damon Swatton on ENVY <damon.swatton@gmail.com>
2023-06-22 20:50:58 +0100

    Add _UIView and use uIViewKey stored within the instance of DataStore associated with UI to calculate the available screen dimensions.

--- lib/ui/ui.dart
+++ lib/ui/ui.dart
@@ -1,10 +1,15 @@
 // Import flutter packages.
 import 'package:flutter/material.dart';
+
+// Import project-specific files.
+import 'package:kar_kam_1/app_data/app_data.dart';
+import 'package:kar_kam_1/app_data/get_it_service.dart';
 import 'package:kar_kam_1/utils/data_store.dart';
+import 'package:kar_kam_1/utils/global_key_extension.dart';
 
 /// Implements a generic page layout design.
 ///
-/// [UI] presents a similar screenlayout for each page with:
+/// [UI] presents a similar screen layout for each page with:
 ///     1. an AppBar at the top with a title,
 ///     2. specific screen contents including buttons for navigation
 ///        and functionality, and
@@ -17,27 +22,92 @@ class UI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Required for calculating [UIViewRect], the available screen dimensions
+    // Required for calculating [uIViewRect], the available screen dimensions
     // via the use of [GlobalKeyExtension.globalPaintBounds].
-    GlobalKey UIViewKey = GlobalKey();
+    GlobalKey uIViewKey = GlobalKey();
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: DataStore<GlobalKey>(
-        key: const ValueKey('UIViewKey'),
-        data: UIViewKey,
-        child: const Placeholder(),
-        // child: _BasePageView(
-        //   key: basePageViewKey,
-        //   pageContents: pageContents,
-        // ),
+        key: const ValueKey('uIViewKey'),
+        data: uIViewKey,
+        child: _UIView(
+          key: uIViewKey,
+          children: const [Placeholder()],
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         color: Theme.of(context).colorScheme.inversePrimary,
         height: 60.0,
       ),
     );
+  }
+}
+
+/// Builds [children] in two parts in order to offer a way for widgets
+/// further down the widget tree to get the available screen dimensions
+/// via the [key] and [globalPaintBounds].
+class _UIView extends StatefulWidget {
+  const _UIView({
+    required Key key,
+    required this.children,
+  }) : super(key: key);
+
+  /// A list of widgets to build on the screen.
+  final List<Widget> children;
+
+  @override
+  State<_UIView> createState() => _UIViewState();
+}
+
+class _UIViewState extends State<_UIView> {
+  /// [children] is updated by [setState] in a post-frame callback.
+  ///
+  /// [children] may depend on knowledge of [uIViewRect] which defines the
+  /// bounding box for [_UIView], hence the reason for the two-part build.
+  List<Widget>? children;
+
+  @override
+  void initState() {
+    // [_UIViewState] is built in two phases:
+    //    (i) with [children] = [Container()], by the [build] method;
+    //    and then
+    //    (ii) with [children] = [widget.children], initiated by
+    //    the following post-frame callback.
+    //
+    // [_UIViewState] is built in two phases because [children] may require
+    // knowledge of the available screen dimensions which this widget attempts
+    // to provide.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Calculate the available screen dimensions and store as [Rect].
+      Rect? uIViewKeyRect =
+          DataStore.of<GlobalKey>(context, const ValueKey('uIViewKey'))
+              .data
+              .globalPaintBounds;
+
+      // Upload [basePageViewRect] to the instance of [AppData]
+      // registered with [GetItService].
+      GetItService.instance<AppData>().update(
+        string: 'uIViewKeyRect',
+        value: uIViewKeyRect,
+        notify: false,
+      );
+
+      // Rebuild widget with [pageSpec.contents] instead of [Container].
+      if (children == null) {
+        setState(() {
+          children = widget.children;
+          // pageContents = [const BasePageViewTest()];
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
