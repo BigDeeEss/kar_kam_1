5efa7ff18441e9e962def53d38f82dbdacfad899
Damon Swatton on ENVY <damon.swatton@gmail.com>
2023-06-23 14:01:39 +0100

    Move KarKam class to main.dart and remove kar_kam.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 // Import external packages.
 import 'package:flutter/material.dart';
+import 'package:get_it_mixin/get_it_mixin.dart';
 
 // Import project-specific files.
 import 'package:kar_kam_1/app_data/app_data.dart';
 import 'package:kar_kam_1/app_data/app_data_preferences_service.dart';
 import 'package:kar_kam_1/app_data/get_it_service.dart';
-import 'package:kar_kam_1/kar_kam/kar_kam.dart';
+import 'package:kar_kam_1/base_ui/base_ui.dart';
 
 /// App start point.
 void main() {
@@ -19,3 +20,48 @@ void main() {
   // Run the app.
   runApp(KarKam());
 }
+
+
+/// [KarKam] is the root widget of this application.
+///
+/// [KarKam] is just a [StatelessWidget] wrapper for an instance of
+/// [FutureBuilder]. This implements a build dependency on the load of app
+/// data from fil.
+class KarKam extends StatelessWidget with GetItMixin{
+  KarKam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kar Kam',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: FutureBuilder<void>(
+        future: GetItService.allReady(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // For the 'has data' case, when the load of app settings
+            // is complete, continue with building BasePage.
+            return const BaseUI();
+          } else {
+            // For the 'has no data' case, where the load of app settings
+            // is still in progress, present a progress indicator.
+            return const Column(
+              // ToDo: Create screen contents for when initialising add data.
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Initialising Kar Kam...',),
+                SizedBox(height: 16,),
+                CircularProgressIndicator(),
+              ],
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
